4372ec51cb3c1f3e8f33fb13b376fc24d9d795db
Rj Florendo <rainflorendo04@gmail.com>
2019-11-14 13:42:56 +0800

    LOGIN OBJECT REPOSIROTY

---
+++ Object Repository/B. Organizational Chart/New Test Object.rs
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>New Test Object</name>
+   <tag></tag>
+   <elementGuidId>d195780d-29f0-4659-a19b-8e0c88759d39</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
